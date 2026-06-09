48bb22155225e808a7390fda83c222879e48ddf8
Husam <husamahmed616@gmail.com>
2026-06-09 17:19:43 +0300

    resize all the val images to fixed diemensions

--- train/train_model.py
+++ train/train_model.py
@@ -285,6 +285,7 @@ def train(config: Config, checkpoint_path=None):
     ])
 
     val_transform = A.Compose([
+        A.Resize(512, 512),
         A.Normalize(
             mean=[0.485, 0.456, 0.406],
             std=[0.229, 0.224, 0.225]
