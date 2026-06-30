8e8a441bed14732dfe4baa690fa4173859e061e8
Husam <husamahmed616@gmail.com>
2026-06-30 16:53:46 +0300

    Fix split to match exactly the paper split

--- train/train_model.py
+++ train/train_model.py
@@ -350,8 +350,8 @@ def train(config: Config, checkpoint_path=None):
     f1_dataset, f2_dataset = random_split(
         fully_sup_train_dataset,
         [
-            len(fully_sup_train_dataset) // 2,
-            len(fully_sup_train_dataset) - len(fully_sup_train_dataset) // 2
+            int(len(fully_sup_train_dataset) * 0.55),
+            len(fully_sup_train_dataset) - int(len(fully_sup_train_dataset) * 0.55)
         ],
         generator=generator
     )
