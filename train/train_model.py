1ea6e199abf315554b8fa85582f66fc9b9431834
Husam <husamahmed616@gmail.com>
2026-07-01 12:31:49 +0300

    back to first split

--- train/train_model.py
+++ train/train_model.py
@@ -341,11 +341,11 @@ def train(config: Config, checkpoint_path=None):
                                     split_ratio=1.0,
                                     transform=train_transform)
     generator = torch.Generator().manual_seed(42)
-    f2_dataset, f1_dataset = random_split(
+    f1_dataset, f2_dataset = random_split(
         fully_sup_train_dataset,
         [
-            int(len(fully_sup_train_dataset) * 0.45),
-            len(fully_sup_train_dataset) - int(len(fully_sup_train_dataset) * 0.45)
+            int(len(fully_sup_train_dataset) * 0.55),
+            len(fully_sup_train_dataset) - int(len(fully_sup_train_dataset) * 0.55)
         ],
         generator=generator
     )
